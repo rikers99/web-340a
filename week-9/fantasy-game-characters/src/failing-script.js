0c62238e0efcee413cfbd3447c301a5c5d81864d
Steve Culmer <culmersteve11@gmail.com>
2024-12-21 08:17:01 -0500

    update

--- week-9/fantasy-game-characters/src/failing-script.js
+++ week-9/fantasy-game-characters/src/failing-script.js
@@ -1,1 +1,15 @@
 // TODO: Implement this script
+"use strict";
+
+const { fork } = require("child_process");
+
+const GameCharacter = ["Fantasy Game Characters"];
+
+GameCharacter.forEach((characters) => {
+ const child = fork("game-characters.js"); // fork a new process
+ child.send(characters); // send the characters to the child process
+
+ child.on("message", (message) => {
+    console.log("Child process for " + characters + " is " + message);
+ });
+});
