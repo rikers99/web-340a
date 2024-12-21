795767dc3c652c39b17d6b7c1ab7de0fcc73948e
Steve Culmer <culmersteve11@gmail.com>
2024-12-21 08:09:50 -0500

    update

--- week-9/fantasy-game-characters/src/game-characters.js
+++ week-9/fantasy-game-characters/src/game-characters.js
@@ -4,10 +4,24 @@ const { spawn } = require("child_process");
 class GameCharacters {
   constructor() {
     // TODO: Set the script file path
+    "use strict";
+    
+    const GameCharacter = ["Fantasy Game Characters"];
+    
+    GameCharacter.forEach((getCharacters) => {
+    console.log("GameCharacter:", getCharacters);
+});
   }
 
   getCharacters(callback) {
     // TODO: Implement this method
+    "use strict";
+    
+    const { spawnSync } = require("child_process");
+    
+    const child = spawnSync("node", ["game-characters.js"]);
+    
+    console.log(child.stdout.toString());
   }
 }
 
